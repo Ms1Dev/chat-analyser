36def5fbe2bd14f79334748f18859da96c1d2ed3
Michael Swann <n1020368@my.ntu.ac.uk>
2026-05-28 21:03:12 +0100

    include the conversation list in the user settings view for sidenav

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,4 @@
-from django.urls import path
-from apps.users.views import AccountSettings
-
 
 app_name = 'users'
 
-urlpatterns = [
-    path('settings/', AccountSettings, name='user-settings')
-]
+urlpatterns = []
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,11 +2,5 @@ from django.shortcuts import render
 from allauth.account.views import PasswordChangeView
 
 
-
-def AccountSettings(request):
-    return render(request, "users/account_settings.html")
-
-
-
 class PasswordChangeOverride(PasswordChangeView):
     template_name = "account/password_set.html"
